dbe7289fffdf22066452e8ddd497e689ca189b12
DESKTOP-A3SCP21\USER <kbrkg118@gmail.com>
2026-03-13 16:56:51 +0900

    digikati(旧:jagkati)の修正

--- digikati/source.js
+++ digikati/source.js
@@ -3,7 +3,15 @@ $(document).ready(function () {
     var colorIndex = ["#ff7f7f","#ff7fff","#ffbfff","#7fffff","#7fff7f","#ffff7f","#ffbf7f"];
     var CounterCnt = 1;
     function updateProbCell($countInput) {
-        var gameValue = parseInt($('.game_input').val()) || 0;
+        var selectedInput = $('input[name="current_input"]:checked').val();
+        var gameValue;
+        if (selectedInput === 'game') {
+            gameValue = parseInt($('.game_input').val()) || 0;
+        } else if (selectedInput === 'memory') {
+            gameValue = parseInt($('.memory_input').val()) || 0;
+        } else {
+            gameValue = 0;
+        }
         
         if (gameValue < 1) {
             $countInput.closest('tr').find('.prob_cell').text('-');
@@ -38,11 +46,47 @@ $(document).ready(function () {
         updateProbCell($countInput);
     })
     $('.game_input').on('change', function() {
+        var selectedInput = $('input[name="current_input"]:checked').val();
+        if (selectedInput === 'game'){
+            $('.count_input').each(function() {
+                updateProbCell($(this));
+            });
+        }
+    });
+    $('.memory_input').on('change', function() {
+        var selectedInput = $('input[name="current_input"]:checked').val();
+        if (selectedInput === 'memory'){
+            $('.count_input').each(function() {
+                updateProbCell($(this));
+            });
+        }
+    });
 
+    $('input[name="current_input"]').on('change', function() {
+        var val = $(this).val();
+        if (val === 'game') {
+            $('#label_for_game').addClass('current_choice');
+            $('#label_for_memory').removeClass('current_choice');
+        } else if (val === 'memory') {
+            $('#label_for_memory').addClass('current_choice');
+            $('#label_for_game').removeClass('current_choice');
+        }
         $('.count_input').each(function() {
             updateProbCell($(this));
         });
     });
+
+    // 初期状態で選択されているラジオボタンに合わせてラベルの色付け
+    (function() {
+        var val = $('input[name="current_input"]:checked').val();
+        if (val === 'game') {
+            $('#label_for_game').addClass('current_choice');
+            $('#label_for_memory').removeClass('current_choice');
+        } else if (val === 'memory') {
+            $('#label_for_memory').addClass('current_choice');
+            $('#label_for_game').removeClass('current_choice');
+        }
+    })();
 
     $('.add_button').on('click', function () {
         var $button = $(this);
